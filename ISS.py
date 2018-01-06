f6afc50ab3a88bfd67dbe95f9041aa332715d62b
Cameron Mukh <cameronmkj@gmail.com>
2018-01-06 03:19:25 +0000

    Added the Geographic Position of the ISS to the Script.

--- ISS.py
+++ ISS.py
@@ -10,3 +10,14 @@ people = result['people']
 
 for p in people:
   print(p['name'])
+
+url = 'http://api.open-notify.org/iss-now.json'
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+
+location = result['iss_position']
+lat = location['latitude']
+lon = location['longitude']
+print('-------------------')
+print('Latitude: ', lat)
+print('Longitude: ', lon)
